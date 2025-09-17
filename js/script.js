30bb9518bdcb3b9c3a609f6dfdd4cc94c8229fb1
Melchizedek Shah <melchizedekshah18@proton.me>
2025-09-16 20:01:53 -0400

    add js folder, css folder, change css file, change html login page

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function togglePassword() {
+  var passwordField = document.getElementById("password");
+  var toggleText = document.querySelector(".toggle-password");
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    toggleText.textContent = "Hide";
+  } else {
+    passwordField.type = "password";
+    toggleText.textContent = "Show";
+  }
+}
