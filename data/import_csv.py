1baee91dc8235e26a214f5752738a881bea7e45d
Melchizedek Shah <melchizedekshah18@proton.me>
2025-10-30 22:08:35 -0400

    Update import_csv.py

--- data/import_csv.py
+++ data/import_csv.py
@@ -10,6 +10,7 @@ fname_config_education = "/home/william/Documents/programming/PWS/NextStep/confi
 fname_config_school = "/home/william/Documents/programming/PWS/NextStep/config/school.csv"
 fname_config_status = "/home/william/Documents/programming/PWS/NextStep/config/status.csv"
 
+
 file_list = [
     (fname_data_import, "data_import"),
     (fname_config_accessibility, "config_accessibility"),
@@ -37,36 +38,27 @@ file_contents = {}
 for name, file_handle in file_handles.items():
     lines = []
     for line in file_handle:
-        lines.append(line.rstrip('\n'))
+        lines.append(line.rstrip("\n"))
     file_contents[name] = lines
 
 # This will be like the list with all the lines of the csv file
 # Need a loop ittiration to get the items
 data_list = []
 
-for line in file_contents['data_import'][1:]: # skip the first line (header)
+for line in file_contents["data_import"][1:]:  # skip the first line (header)
     data = line.split(",")
-    cleaned_data = [item.strip().strip('"\'') for item in data]
+    cleaned_data = [item.strip().strip("\"'") for item in data]
     data_list.append(cleaned_data)
-
-# Example
-# How to access data
-#print(data_list)
-#print(data_list[0][3])
-#print(file_contents['config_city'])
-#print(file_contents['config_class'])
-#print(file_contents['data_import'])
 
 ########################################################
 # From this point all the data is in list or dictionaries
-# The next part will make shure that everie enry of the
-# import data is a valid entry
+# The next part will make shure that every enry of the
+# import data is a valid entry - So it is verification
 # ######################################################
 
-#time ,  email1, email2, name, phone, class, country, city, school, education_program, status, accessibility
+# time ,  email1, email2, name, phone, class, country, city, school, education_program, status, accessibility
 # 0        1        2      3      4      5      6        7     8            9             10          11
 # This is the csv standart used (from googled docs, with google email requierd)
-
 
 
 def compare_data(data_number, config_name):
@@ -75,28 +67,52 @@ def compare_data(data_number, config_name):
         if person[data_number] == class_name:
             data_exists = True
     if data_exists == True:
-        #print("data supported")
+        # print("data supported")
         data_exists = False
         return True
-    else :
-        print(f"ERROR - {person[3]} has a invalid data value that does not exist in {config_name}")
+    else:
+        # print(f"ERROR - {person[3]} has a invalid data value that does not exist in {config_name}")
         return False
 
-result_person = False
+
+result_verification = True
 for person in data_list:
-    if compare_data(5, "config_class") == True and \
-    compare_data(6, "config_country") == True and \
-    compare_data(7, "config_city") == True and \
-    compare_data(8, "config_school") == True and \
-    compare_data(9, "config_education") == True and \
-    compare_data(10, "config_status") == True and \
-    compare_data(11, "config_accessibility") == True:
-        result_person = True
+    if (
+        compare_data(5, "config_class") == False
+        or compare_data(6, "config_country") == False
+        or compare_data(7, "config_city") == False
+        or compare_data(8, "config_school") == False
+        or compare_data(9, "config_education") == False
+        or compare_data(10, "config_status") == False
+        or compare_data(11, "config_accessibility") == False
+    ):
+        result_verification = False
 
-    if result_person == True:
-        print(f"SUCCES - {person[3]}")
+
+if result_verification == True:
+    print(f"SUCCES - all the data is verified")
+else:
+    print("ERROR - invalid data")
 
 
-# Loop that closes all the files
-for name, f in file_handles.items():
-    f.close()
+# funtion that closes all the files
+def close_all_files():
+    for name, f in file_handles.items():
+        f.close()
+
+
+# Popluate database
+import sqlite3
+# opening the database
+try:
+    conn = sqlite3.connect('/home/william/Documents/programming/PWS/NextStep/setup/nextstep_data.db') # this is temporary !!!!!!!
+    cursor = conn.cursor()
+    print("connection succes")
+except:
+    print("error opening database")
+    # loop that closes all the files
+    close_all_files()
+    exit()
+
+
+close_all_files()
