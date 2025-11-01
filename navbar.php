a91f39ba2e0263e9d4afb579f960045940bab89b
Melchizedek Shah <melchizedekshah18@proton.me>
2025-10-31 20:30:26 -0400

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+<h1><a href="index.php" class="brand-name">NextStep</a></h1>
+<div class="nav-buttons">
+<a href="settings.php" class="nav-btn">Settings</a>
+<a href="import_export.php" class="nav-btn">Data</a>
+<a href="map.php" class="nav-btn">Map</a>
+<a href="logout.php" class="nav-btn">Log out</a>
+</div>
+</nav>
