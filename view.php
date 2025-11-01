c9276bac794e069c12e6a4826be5c1b0bcf34036
Melchizedek Shah <melchizedekshah18@proton.me>
2025-11-01 10:45:36 -0400

    Update view.php

--- view.php
+++ view.php
@@ -48,9 +48,7 @@ if (!$stmt) {
     errorMessages("Error preparing query", $db->lastErrorMsg());
 }
 
-
 $stmt->bindValue(":id", $_GET["student_id"], SQLITE3_INTEGER);
-
 
 $results = $stmt->execute();
 if (!$results) {
@@ -64,6 +62,20 @@ if (!$row) {
     exit();
 }
 
+$student_name          = htmlspecialchars($row['students_name']);
+$student_email         = htmlspecialchars($row['students_email']);
+$student_phone_number  = htmlspecialchars($row['students_phone_number']);
+$class_name            = htmlspecialchars($row['class_name']);
+$country_name          = htmlspecialchars($row['country_name']);
+$city_name             = htmlspecialchars($row['city_name']);
+$school_name           = htmlspecialchars($row['school_name']);
+$program_name          = htmlspecialchars($row['program_name']);
+$status                = htmlspecialchars($row['status_name']);
+$accessibility         = htmlspecialchars($row['accessibility_name']);
+$created_date          = htmlspecialchars($row['students_created_date']);
+$last_update           = htmlspecialchars($row['students_last_updated']);
+
+$db->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -73,9 +85,30 @@ if (!$row) {
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link rel="icon" type="image/x-icon" href="images/logo.webp"/>
 <link rel="stylesheet" href="css/style_navbar.css"/>
+<link rel="stylesheet" href="css/style_view.css"/>
 <title>NextStep - View</title>
 </head>
 <body>
-    <?php include 'navbar.php'; ?>
+<?php include 'navbar.php'; ?>
+
+<div class="page-box">
+<h2>Student info</h2>
+<p>Name: <strong><?=$student_name?></strong></p>
+<p>Email: <strong><?=$student_email?></strong></p>
+<p>Phone number: <strong><?=$student_phone_number?></strong></p>
+<p>Class name: <strong><?=$class_name?></strong></p>
+<p>Country: <strong><?=$country_name?></strong></p>
+<p>City: <strong><?=$city_name?></strong></p>
+<p>School: <strong><?=$school_name?></strong></p>
+<p>Program: <strong><?=$program_name?></strong></p>
+<p>Status: <strong><?=$status?></strong></p>
+<p>Accessibility: <strong><?=$accessibility?></strong></p>
+<p>Date created: <strong><?=$created_date?></strong></p>
+<p>Date last update: <strong><?=$last_update?></strong></p>
+<a href="edit.php" class="nav-btn">Edit</a>
+<a href="delete.php" class="nav-btn">Delete</a>
+
+
+</div>
 </body>
 </html>
