390447919226d0c9d6450011bd08dc39cc0f2063
Melchizedek Shah <melchizedekshah18@proton.me>
2025-11-01 16:20:56 -0400

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -2,6 +2,7 @@
 <h1><a href="index.php" class="brand-name">NextStep</a></h1>
 <div class="nav-buttons">
 <a href="settings.php" class="nav-btn">Settings</a>
+<a href="teachers.php" class="nav-btn">Teachers</a>
 <a href="import_export.php" class="nav-btn">Data</a>
 <a href="map.php" class="nav-btn">Map</a>
 <a href="logout.php" class="nav-btn">Log out</a>
