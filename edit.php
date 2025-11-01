4d8f5d4f9cea7348b32769cdd13cfdfd8a60c4c9
Melchizedek Shah <melchizedekshah18@proton.me>
2025-11-01 14:14:35 -0400

    Update pages

--- edit.php
+++ edit.php
@@ -1,3 +1,36 @@
+<?php
+require_once "utils.php";
+session_start();
+loginSecurity();
+if ($_SESSION["teacher_username"] != "ADMIN") {
+    header("Location: index.php");
+    exit();
+}
+check_id();
+
+$row = full_students_database_query($db_file);
+
+if (!$row) {
+    $_SESSION['error'] = 'Invalid value for student_id';
+    header('Location: index.php');
+    exit();
+}
+
+$student_id            = htmlspecialchars($row['students_id']);
+$student_name          = htmlspecialchars($row['students_name']);
+$student_email         = htmlspecialchars($row['students_email']);
+$student_phone_number  = htmlspecialchars($row['students_phone_number']);
+$class_name            = htmlspecialchars($row['class_name']);
+$country_name          = htmlspecialchars($row['country_name']);
+$city_name             = htmlspecialchars($row['city_name']);
+$school_name           = htmlspecialchars($row['school_name']);
+$program_name          = htmlspecialchars($row['program_name']);
+$status                = htmlspecialchars($row['status_name']);
+$accessibility         = htmlspecialchars($row['accessibility_name']);
+$created_date          = htmlspecialchars($row['students_created_date']);
+$last_update           = htmlspecialchars($row['students_last_updated']);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +39,39 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link rel="icon" type="image/x-icon" href="images/logo.webp"/>
 <link rel="stylesheet" href="css/style_navbar.css"/>
+<link rel="stylesheet" href="css/style_page.css"/>
 <title>NextStep - Settings</title>
 </head>
 <body>
-    <?php include 'navbar.php'; ?>
+<?php include 'navbar.php'; ?>
+<div class="page-box">
+<h2>Student information</h2>
+<label for="student_name">Name:</label>
+<input type="text" id="student_name" value="<?=$student_name?>" />
+<label for="student_email">Email:</label>
+<input type="email" id="student_email" value="<?=$student_email?>" />
+<label for="student_phone">Phone number:</label>
+<input type="tel" id="student_phone" value="<?=$student_phone_number?>" />
+<label for="class_name">Class name:</label>
+<input type="text" id="class_name" value="<?=$class_name?>" />
+<label for="country_name">Country:</label>
+<input type="text" id="country_name" value="<?=$country_name?>" />
+<label for="city_name">City:</label>
+<input type="text" id="city_name" value="<?=$city_name?>" />
+<label for="school_name">School:</label>
+<input type="text" id="school_name" value="<?=$school_name?>" />
+<label for="program_name">Program:</label>
+<input type="text" id="program_name" value="<?=$program_name?>" />
+<label for="status">Status:</label>
+<input type="text" id="status" value="<?=$status?>" />
+<label for="accessibility">Accessibility:</label>
+<input type="text" id="accessibility" value="<?=$accessibility?>" />
+<label for="created_date">Date created:</label>
+<input type="text" id="created_date" value="<?=$created_date?>" />
+<label for="last_update">Date last update:</label>
+<input type="text" id="last_update" value="<?=$last_update?>" />
+<input type="submit" class="nav-btn" name="save" value="Save">
+</div>
+
 </body>
 </html>
--- view.php
+++ view.php
@@ -2,66 +2,16 @@
 require_once "utils.php";
 session_start();
 loginSecurity();
+check_id();
 
-try {
-    $db = new SQLite3($db_file);
-} catch (Exception $e) {
-    errorMessages("Database connection failed", $e->getMessage());
-}
-
-if (!isset($_GET['student_id'])) {
-    $_SESSION['error'] = "Missing student_id";
-    header("Location: index.php");
-    exit();
-}
-if (!is_numeric($_GET['student_id'])) {
-    $_SESSION['error'] = "Invalid value for student_id";
-    header("Location: index.php");
-    exit();
-}
-
-$query = "SELECT
-    STUDENTS.students_name,
-    STUDENTS.students_email,
-    STUDENTS.students_phone_number,
-    CLASS.class_name,
-    COUNTRY.country_name,
-    CITY.city_name,
-    SCHOOL.school_name,
-    EDUCATION_PROGRAM.program_name,
-    STATUS.status_name,
-    ACCESSIBILITY.accessibility_name,
-    STUDENTS.students_created_date,
-    STUDENTS.students_last_updated
-FROM STUDENTS
-LEFT JOIN CLASS ON STUDENTS.students_class_id = CLASS.class_id
-LEFT JOIN COUNTRY ON STUDENTS.students_country_id = COUNTRY.country_id
-LEFT JOIN CITY ON STUDENTS.students_city_id = CITY.city_id
-LEFT JOIN SCHOOL ON STUDENTS.students_school_id = SCHOOL.school_id
-LEFT JOIN EDUCATION_PROGRAM ON STUDENTS.students_education_program_id = EDUCATION_PROGRAM.program_id
-LEFT JOIN STATUS ON STUDENTS.students_status_id = STATUS.status_id
-LEFT JOIN ACCESSIBILITY ON STUDENTS.students_accessibility_id = ACCESSIBILITY.accessibility_id
-WHERE STUDENTS.students_id = :id;";
-
-$stmt = $db->prepare($query);
-if (!$stmt) {
-    errorMessages("Error preparing query", $db->lastErrorMsg());
-}
-
-$stmt->bindValue(":id", $_GET["student_id"], SQLITE3_INTEGER);
-
-$results = $stmt->execute();
-if (!$results) {
-    errorMessages("Error executing query", $db->lastErrorMsg());
-}
-$row = $results->fetchArray();
+$row = full_students_database_query($db_file);
 
 if (!$row) {
     $_SESSION['error'] = 'Invalid value for student_id';
     header('Location: index.php');
     exit();
 }
-
+$student_id            = htmlspecialchars($row['students_id']);
 $student_name          = htmlspecialchars($row['students_name']);
 $student_email         = htmlspecialchars($row['students_email']);
 $student_phone_number  = htmlspecialchars($row['students_phone_number']);
@@ -75,7 +25,6 @@ $accessibility         = htmlspecialchars($row['accessibility_name']);
 $created_date          = htmlspecialchars($row['students_created_date']);
 $last_update           = htmlspecialchars($row['students_last_updated']);
 
-$db->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -85,14 +34,14 @@ $db->close();
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link rel="icon" type="image/x-icon" href="images/logo.webp"/>
 <link rel="stylesheet" href="css/style_navbar.css"/>
-<link rel="stylesheet" href="css/style_view.css"/>
+<link rel="stylesheet" href="css/style_page.css"/>
 <title>NextStep - View</title>
 </head>
 <body>
 <?php include 'navbar.php'; ?>
 
 <div class="page-box">
-<h2>Student info</h2>
+<h2>Student information</h2>
 <p>Name: <strong><?=$student_name?></strong></p>
 <p>Email: <strong><?=$student_email?></strong></p>
 <p>Phone number: <strong><?=$student_phone_number?></strong></p>
@@ -105,10 +54,12 @@ $db->close();
 <p>Accessibility: <strong><?=$accessibility?></strong></p>
 <p>Date created: <strong><?=$created_date?></strong></p>
 <p>Date last update: <strong><?=$last_update?></strong></p>
-<a href="edit.php" class="nav-btn">Edit</a>
-<a href="delete.php" class="nav-btn">Delete</a>
-
-
+<?php
+if ($_SESSION["teacher_username"] == "ADMIN") {
+    echo '<a href=edit.php?student_id='.$student_id.' class="nav-btn">Edit</a>
+        <a href="delete.php" class="nav-btn">Delete</a>';
+}
+?>
 </div>
 </body>
 </html>
