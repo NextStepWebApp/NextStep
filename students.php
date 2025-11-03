ac21ea7c47f4f0cfa6ebb169feae504619a7a610
Melchizedek Shah <melchizedekshah18@proton.me>
2025-11-03 13:48:28 -0400

    Rename to students

---
+++ students.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="icon" type="image/x-icon" href="images/logo.webp"/>
+<link rel="stylesheet" href="css/style_navbar.css"/>
+<link rel="stylesheet" href="css/style_page.css"/>
+<title>NextStep - Student</title>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+
+<div class="page-box">
+
+<a href="create_student.php" class="nav-btn">Create Student</a>
+<a href="import.php" class="nav-btn">Import Data</a>
+<a href="import.php" class="nav-btn">Export Data</a>
+
+
+</div>
+</body>
+</html>
