befad1638e9b9c279314e541458b1df0fcdbe3f3
Melchizedek Shah <melchizedekshah18@proton.me>
2025-11-09 19:26:34 -0400

    Update teachers (need fix)

--- teachers.php
+++ teachers.php
@@ -1,62 +1,80 @@
 <?php
+require_once "utils.php";
 session_start();
 loginSecurity();
 super_user_privilages($_SESSION["teacher_username"]);
 
 $db = new SQLite3($db_file);
 
-
+# Fetch all teachers
+$query = "SELECT teacher_id, teacher_name, teacher_username, teacher_email FROM TEACHERS";
+$stmt = $db->prepare($query);
+if (!$stmt) {
+    errorMessages("Error preparing query", $db->lastErrorMsg());
+}
+$results = $stmt->execute();
+if (!$results) {
+    errorMessages("Error executing query", $db->lastErrorMsg());
+}
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
 <head>
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link rel="icon" type="image/x-icon" href="images/logo.webp"/>
+<link rel="stylesheet" href="css/style_page.css"/>
 <link rel="stylesheet" href="css/style_navbar.css"/>
-<title>NextStep - Settings</title>
+<title>NextStep - Teachers</title>
 </head>
 <body>
-    <?php include 'navbar.php'; ?>
-    <?php flashMessages();?>
-    <div class="table-container">
-    <table>
-    <thead>
-    <tr>
-    <th>Select</th>
-    <th>Name</th>
-    <th>Email</th>
-    <th>Status</th>
-    <th>Date</th>
-    </tr>
-    </thead>
-    <tbody id="tableBody">
-    <?php
-    $hasresults = 0;
-    while ($row = $results->fetchArray()) {
-        $hasresults = 1;
-        $student_id = htmlspecialchars($row["students_id"]);
-        $date = htmlspecialchars($row["students_created_date"]);
-        $name = htmlspecialchars($row["students_name"]);
-        $email = htmlspecialchars($row["students_email"]);
-        $status = htmlspecialchars($row["status_name"]);
-        echo '<tr>
-            <td><input type="checkbox" class="row-checkbox"/></td>
-            <td><a href=view.php?student_id='.$student_id.'>'.$name.'</a></td>
-            <td>'.$email.'</td>
-            <td>'.$status.'</td>
-            <td>'.$date.'</td>
-            </tr>'."\n";
+<?php include 'navbar.php'; ?>
+<section class="table-section"> 
+<?php flashMessages();?>
+<div class="teacher-button">
+<a href="create_teacher.php" class="nav-btn">Create teacher</a>
+</div>
+<div class="table-container">
+<table>
+<thead>
+<tr>
+<th>Name</th>
+<th>Username</th>
+<th>Email</th>
+<th>Actions</th>
+</tr>
+</thead>
+<tbody id="tableBody">
+<?php
+while ($row = $results->fetchArray()) {
+    $name = htmlspecialchars($row["teacher_name"]);
+    $username = htmlspecialchars($row["teacher_username"]);
+    $email = htmlspecialchars($row["teacher_email"]);
+    $id = $row["teacher_id"];
+    
+    echo '<tr>
+            <td>' . $name . '</td>
+            <td>' . $username . '</td>
+            <td>' . $email . '</td>
+            <td>
+                <a href="edit_teacher.php?teacher_id=' . $id . '" class="nav-btn">Edit</a>';
+                
+    if ($username != "ADMIN") {
+        echo '<a href="delete.php?teacher_id=' . $id . '" class="nav-btn">Delete</a>';
     }
-    if ($hasresults == 0)
-        echo('<tr><td colspan="5" class="no-records">No records found</td></tr>');
-    $db->close();
-    ?>
-    </tbody>
-    </table>
-    </div>
-
+    if(isset($_SESSION["new_teacher_credentials"])) {
+        echo '<a href="teacher_download.php" class="nav-btn">Download</a>';
+    }
+    
+    echo '    </td>
+        </tr>';
+}
+$db->close();
+?>
+</tbody>
+</table>
+</div>
+</section>
 </body>
 </html>
