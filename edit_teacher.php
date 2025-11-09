b9c7952541ac01a2b260ba9e09cafdbb0b095072
Melchizedek Shah <melchizedekshah18@proton.me>
2025-11-09 19:25:42 -0400

    Create edit_teacher.php

---
+++ edit_teacher.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="icon" type="image/x-icon" href="images/logo.webp"/>
+<link rel="stylesheet" href="css/style_navbar.css"/>
+<title>NextStep - Edit Teacher</title>
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+</body>
+</html>
