4b8ccd29bf508baf5b4b955410848531fba3e05b
Melchizedek Shah <melchizedekshah18@proton.me>
2025-11-09 16:08:02 -0400

    Update teachers.php

--- teachers.php
+++ teachers.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+loginSecurity();
+super_user_privilages($_SESSION["teacher_username"]);
+
+$db = new SQLite3($db_file);
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,5 +20,43 @@
 </head>
 <body>
     <?php include 'navbar.php'; ?>
+    <?php flashMessages();?>
+    <div class="table-container">
+    <table>
+    <thead>
+    <tr>
+    <th>Select</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Status</th>
+    <th>Date</th>
+    </tr>
+    </thead>
+    <tbody id="tableBody">
+    <?php
+    $hasresults = 0;
+    while ($row = $results->fetchArray()) {
+        $hasresults = 1;
+        $student_id = htmlspecialchars($row["students_id"]);
+        $date = htmlspecialchars($row["students_created_date"]);
+        $name = htmlspecialchars($row["students_name"]);
+        $email = htmlspecialchars($row["students_email"]);
+        $status = htmlspecialchars($row["status_name"]);
+        echo '<tr>
+            <td><input type="checkbox" class="row-checkbox"/></td>
+            <td><a href=view.php?student_id='.$student_id.'>'.$name.'</a></td>
+            <td>'.$email.'</td>
+            <td>'.$status.'</td>
+            <td>'.$date.'</td>
+            </tr>'."\n";
+    }
+    if ($hasresults == 0)
+        echo('<tr><td colspan="5" class="no-records">No records found</td></tr>');
+    $db->close();
+    ?>
+    </tbody>
+    </table>
+    </div>
+
 </body>
 </html>
