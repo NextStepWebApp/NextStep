a6f66ce5bee1ad99043e12e3c769324338801192
Melchizedek Shah <melchizedekshah18@proton.me>
2026-02-06 18:32:56 -0400

    Fix map page

--- map/index.php
+++ map/index.php
@@ -1,14 +1,30 @@
-<!DOCTYPE html>
+<?php
+require_once "../utils.php";
+setup_checker();
+session_start();
+loginSecurity();
+
+try {
+    $db = new SQLite3($db_file);
+} catch (Exception $e) {
+    errorMessages("Database connection failed", $e->getMessage());
+}
+
+$color_theme = color_theme_helper($db, $color_theme_system["theme_color"]);
+?>
+<!doctype html>
 <html lang="en">
-<head>
 <head>
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<link rel="icon" type="image/x-icon" href="../images/logo.webp"/>
+<link rel="icon" type="image/x-icon" href="images/logo.webp"/>
 <link rel="stylesheet" href="../css/style_navbar.css"/>
-<title>NextStep - Map</title>
+<link rel="stylesheet" href="../css/style_page.css"/>
+<title>NextStep - Map </title>
 </head>
-<body class="theme-<?= $color_theme["theme_color"] ?>">
-    <?php include "../navbar.php"; ?>
+<body class="theme-<?= $color_theme ?>">
+<?php include "../navbar.php"; ?>
+<?php flashMessages(); ?>
+<script src="../js/script.js"></script>
 </body>
 </html>
